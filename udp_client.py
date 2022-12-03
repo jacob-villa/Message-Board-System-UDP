c4b61f24993efd4c7b006e3ecbf10da48569afce
Jacob Villa <80886887+jacob-villa@users.noreply.github.com>
2022-12-03 21:40:05 +0800

    refactor client and server
    new files

--- udp_client.py
+++ udp_client.py
@@ -1,6 +1,6 @@
 import socket
 import threading
-import random
+from typing import List
 
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -11,9 +11,9 @@ server_port = None
 
 bufferSize = 1024
 
-clientSocket.bind((server_IP, random.randint(8000, 9000)))
+# clientSocket.bind((server_IP, random.randint(8000, 9000)))
 
-clientName = input("Name: ")
+# clientName = input("Name: ")
 
 def receive():
     while True:
@@ -23,7 +23,7 @@ def receive():
         except:
             pass
 
-def concatMessage(clientCommand: list):
+def concatMessage(clientCommand: List[str]) -> List[str]:
     command = list(clientCommand[0])
 
     if clientCommand[0] == "/all":
@@ -38,28 +38,70 @@ def concatMessage(clientCommand: list):
 
     return command
 
+def errorCheck(clientCommand: list):
+    # Error checking for command syntax
+    if clientCommand[0] not in ["/join", "/leave", "/register", "/all", "/msg", "/?"]:
+        print("Error: Command not found.")
+        return False
+
+    # Checking errors for /join
+    if clientCommand[0] == "/join":
+        if not clientCommand[2].isDigit():
+            print("Error: Connection to the Message Board Server has failed! Please check IP Address and Port Number.")
+            return False
+
+    # Checking errors for /leave
+    if clientCommand[0] == "/leave":
+        if server_IP is None or server_port is None:
+            print("Error: Disconnection failed. Please connect to the server first.")
+            return False
+        elif len(clientCommand) > 1:
+            print("Error: Command parameters do not match or is not allowed.")
+            return False
+
+    # Checking errors for /register
+    if clientCommand[0] == "/register" and len(clientCommand) > 2:
+        print("Error: Command parameters do not match or is not allowed.")
+        return False
+
+    # Checking errors for /?
+    if clientCommand[0] == "/?" and len(clientCommand) > 1:
+        print("Error: Command parameters do not match or is not allowed.")
+        return False
+
+    return True
+    
 
 thread = threading.Thread(target = receive)
 thread.start()
 
-clientSocket.sendto(f"SIGNUP_TAG:{clientName}".encode(), (server_IP, server_port))
+# clientSocket.sendto(f"SIGNUP_TAG:{clientName}".encode(), (server_IP, server_port))
 
-print("UDP target IP: ", server_IP)
-print("UDP target Port: ", server_port)
+# print("UDP target IP: ", server_IP)
+# print("UDP target Port: ", server_port)
 
 while True:
+    # input from cli
     clientMsg = input("")
     clientCommand = clientMsg.strip(' ').split(' ')
 
+    # input:  clientMsg = "/all hello world"
+    # output: clientCommand = ["/all", "hello world"]
+    # input:  clientMsg = "/msg Deckard hello world again"
+    # output: clientCommand = ["/msg", "Deckard", "hello world again"]
     if clientCommand[0] == "/all" or clientCommand[0] == "/msg":
         clientCommand = concatMessage(clientCommand)
+
+    #if errorCheck(clientCommand):
+
 
     if clientCommand[0] == "/join":
         server_IP = clientCommand[1]
         server_port = clientCommand[2]
 
+
     if clientCommand[0] == "/leave":
         print("Connection closed. Thank you!")
         exit()
-    else:
-        clientSocket.sendto(f"{clientName}: {clientMsg}".encode(), (server_IP, server_port))
+    # else:
+        # clientSocket.sendto(f"{clientName}: {clientMsg}".encode(), (server_IP, server_port))
--- udp_server.py
+++ udp_server.py
@@ -1,56 +1,91 @@
+import json
 import socket
 import threading
 import queue
 
+# Client class
+class client:
+    def __init__(self, addr: tuple, handle: str = None):
+        self.addr = addr
 
-messagesQueue = queue.Queue()
-clientsList = []
 
-serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# Messages and list of clients
+msg_queue = queue.Queue()
+client_list = []
 
-server_IP = socket.gethostname()
-server_port = 20001
 
+# Initialize server socket
+server_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bufferSize = 1024
 
-serverSocket.bind((server_IP, server_port))
+
+# Server details and socket binding
+server_addr = socket.gethostname()
+server_port = 20001
+server_sock.bind((server_addr, server_port))
 
 
-def receive():
+def recv_from_clients():
     while True:
         try:
-            print("Waiting for client...")
-            
-            msg, addr = serverSocket.recvfrom(bufferSize)
-            messagesQueue.put((msg, addr))
-
+            client_msg, client_addr = server_sock.recvfrom(bufferSize)
+            msg_queue.put((client_msg.decode(), client_addr))
         except:
             pass
 
-def broadcast():
+
+def create_reply(msg: str, addr: tuple) -> dict:
+    global client_list
+    
+    reply = {}
+    
+    # Get message in dict format
+    msg_json = json.loads(msg)
+
+    # Get command from message
+    cmd = msg_json["command"]
+
+    # Receive /join from client
+    if cmd == "join":
+        if any(client for client in client_list if client.addr == addr):
+            reply["command"] = "error"
+            reply["message"] = "Current client has already joined the server."
+        else:
+            reply["command"] = "join"
+    elif cmd == "leave":
+        if any(client for client in client_list if client.addr == addr):
+            # NOTE: Replaces the existing client_list with a new object 
+            client_list = [client for client in client_list if client.addr != addr]
+            reply["command"] = "leave"
+
+        #TODO: Make replies for all other commands, figure out how to send to recipient
+
+    return reply
+
+
+def send_to_clients():
     while True:
-        while not messagesQueue.empty():
-            currentMessage, currentAddr = messagesQueue.get()
+        while not msg_queue.empty():
+            current_msg, current_addr = msg_queue.get()
 
-            print(currentMessage.decode())
-            print("Client IP Address: {}".format(currentAddr))
+            if not any(client for client in client_list if client.addr == current_addr):
+                client_list.append(client(current_addr))
 
-            if currentAddr not in clientsList:
-                clientsList.append(currentAddr)
-
-            for client in clientsList:
+            """ for client in client_list:
                 try:
-                    if currentMessage.decode("utf-8").startswith("SIGNUP_TAG:"):
-                        clientName = currentMessage.decode()[currentMessage.decode().index(":") + 1 : ]
-                        serverSocket.sendto(f"Welcome {clientName}!".encode(), client)
+                    if current_msg.decode("utf-8").startswith("SIGNUP_TAG:"):
+                        clientName = current_msg.decode()[current_msg.decode().index(":") + 1 : ]
+                        server_sock.sendto(f"Welcome {clientName}!".encode(), client)
                     else:
-                        serverSocket.sendto(currentMessage, client)
+                        server_sock.sendto(current_msg, client)
                 except:
-                    clientsList.remove(client)
+                    client_list.remove(client) """
+
+            # Call create_reply(), turn return val into JSON with json.dumps, sendto client
 
 
-receiveThread = threading.Thread(target = receive)
-broadcastThread = threading.Thread(target = broadcast)
+recv_thread = threading.Thread(target = recv_from_clients)
+send_thread = threading.Thread(target = send_to_clients)
 
-receiveThread.start()
-broadcastThread.start()
+recv_thread.start()
+send_thread.start()
