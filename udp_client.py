be78e1b2698df2529352f237bf877f40711b229c
Jacob Villa <80886887+jacob-villa@users.noreply.github.com>
2022-12-12 15:32:16 +0800

    finished submittable project
    completed basic functionalities required for the project

--- udp_client.py
+++ udp_client.py
@@ -1,107 +1,267 @@
+import json
+import random
 import socket
 import threading
-from typing import List
+import time
 
-clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# server_IP = socket.gethostbyname(socket.gethostname())
-# server_port = 20001
-server_IP = None
+# Initialize UDP client socket
+client_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client_sock.settimeout(5)
+buffer_size = 1024
+
+
+# Server connection details
+server_addr = None
 server_port = None
 
-bufferSize = 1024
+client_sock.bind(('localhost', random.randint(8000, 9000)))
 
-# clientSocket.bind((server_IP, random.randint(8000, 9000)))
+# Client message for store-and-forward for echoing unicast messages
+client_message = None
 
-# clientName = input("Name: ")
+# Boolean flag to check if connected to server
+connected_to_server = False
 
-def receive():
+# Emoji dict for bonus points :)
+emoji_dict = {
+    ":heart:": "<3",
+    ":rat:": "<:3~",
+    ":sad:": ":c",
+    ":happy:": "c:",
+    ":blush:": "૮ ˶ᵔ ᵕ ᵔ˶ ა",
+    ":angel:":  "ପ(๑•ᴗ•๑)ଓ ♡",
+    ":pout:": "૮₍ ˃ ⤙ ˂ ₎ა"
+}
+
+
+def replace_emojis(msg: list):
+    for i in range(len(msg)):
+        if msg[i] in emoji_dict:
+            msg[i] = emoji_dict[msg[i]]
+
+    return msg
+
+# Main program logic
+def send_to_server(cmd: str, handle: str = None, msg: str = None):
+    global client_message
+    
+    # Build command object
+    message = {
+        "command": cmd
+    }
+    if handle is not None:
+        message["handle"] = handle
+    if msg is not None:
+        message["message"] = msg
+    
+    client_message = message
+
+    # Encode command object as string
+    message_json = json.dumps(message)
+
+    # Send JSON-encoded command to server
+    try:
+        client_sock.sendto(message_json.encode(), (server_addr, server_port))
+    except:
+        print("Error: Not yet connected to a server.")
+
+
+def recv_from_server():
+    global client_message, connected_to_server
+    
     while True:
         try:
-            msg, addr = clientSocket.recvfrom(bufferSize)
-            print(msg.decode())
+            # Wait for response
+            response_packet, _ = client_sock.recvfrom(buffer_size)
+            response_decoded = response_packet.decode()
+
+            # Decode server response JSON
+            response = json.loads(response_decoded)
+
+            # Parse command
+            cmd = response["command"]
+            if cmd == "join":
+                print("Connection to the Message Board Server is successful!")
+                connected_to_server = True
+            elif cmd == "leave":
+                print("Connection closed. Thank you!")
+                connected_to_server = False
+            elif cmd == "register":
+                print("Welcome {0}!".format(response["handle"]))
+            elif cmd == "all":
+                print("{0}: {1}".format(response["handle"], response["message"]))
+            elif cmd == "msg":
+                # Echo the sent message if the response is equal to the stored message
+                if response == client_message:
+                    print("[To {0}]: {1}".format(response["handle"], response["message"]))
+                    client_message = None
+                # Receiving a message from another client
+                else:
+                    print("[From {0}]: {1}".format(response["handle"], response["message"]))
+            elif cmd == "error":
+                print("Error: {0}".format(response["message"]))
         except:
             pass
 
-def concatMessage(clientCommand: List[str]) -> List[str]:
-    command = list(clientCommand[0])
 
-    if clientCommand[0] == "/all":
-        message = ' '.join(clientCommand[1 : ])
-        command.append(message)
-
-    elif clientCommand[0] == "/msg":
-        handle = clientCommand[1]
-        message = ' '.join(clientCommand[2 : ])
-        command.append(handle)
-        command.append(message)
-
-    return command
-
-def errorCheck(clientCommand: list):
+def error_check(cmd: str, args: list) -> bool:
     # Error checking for command syntax
-    if clientCommand[0] not in ["/join", "/leave", "/register", "/all", "/msg", "/?"]:
+    if cmd not in ["/join", "/leave", "/register", "/all", "/msg", "/?", "/emojis"]:
         print("Error: Command not found.")
         return False
 
     # Checking errors for /join
-    if clientCommand[0] == "/join":
-        if not clientCommand[2].isDigit():
+    if cmd == "/join":
+        if len(args) != 2 or not args[1].isdigit():
             print("Error: Connection to the Message Board Server has failed! Please check IP Address and Port Number.")
+            return False
+        else:
+            try:
+                socket.inet_aton(args[0])
+            except:
+                print("Error: Connection to the Message Board Server has failed! Please check IP Address and Port Number.")
+                return False
+
+        if server_addr is not None and server_port is not None:
+            print("Error: Already connected to a server.")
             return False
 
     # Checking errors for /leave
-    if clientCommand[0] == "/leave":
-        if server_IP is None or server_port is None:
+    if cmd == "/leave":
+        if server_addr is None or server_port is None:
             print("Error: Disconnection failed. Please connect to the server first.")
             return False
-        elif len(clientCommand) > 1:
+        elif len(args) > 0:
             print("Error: Command parameters do not match or is not allowed.")
             return False
 
     # Checking errors for /register
-    if clientCommand[0] == "/register" and len(clientCommand) > 2:
+    if cmd == "/register" and len(args) != 1:
         print("Error: Command parameters do not match or is not allowed.")
         return False
 
+    # Checking errors for /all
+    if cmd == "/all" and (len(args) < 1 or args[0] == ""):
+        print("here1Error: Command parameters do not match or is not allowed.")
+        return False
+
+    # Checking errors for /msg
+    if cmd == "/msg" and len(args) < 2:
+        print("here2Error: Command parameters do not match or is not allowed.")
+        return False
+
     # Checking errors for /?
-    if clientCommand[0] == "/?" and len(clientCommand) > 1:
+    if cmd == "/?" and len(args) > 0:
         print("Error: Command parameters do not match or is not allowed.")
         return False
 
     return True
-    
 
-thread = threading.Thread(target = receive)
-thread.start()
+# Creating thread for receiving replies from server
+recv_thread = threading.Thread(target = recv_from_server)
+recv_thread.daemon = True
+recv_thread.start()
 
-# clientSocket.sendto(f"SIGNUP_TAG:{clientName}".encode(), (server_IP, server_port))
 
-# print("UDP target IP: ", server_IP)
-# print("UDP target Port: ", server_port)
-
+# Main loop for receiving CLI input
 while True:
-    # input from cli
-    clientMsg = input("")
-    clientCommand = clientMsg.strip(' ').split(' ')
+    # Input from CLI
+    try:
+        client_message = input()
+    except KeyboardInterrupt:
+        try:
+            send_to_server("leave")
+        except:
+            pass
+        break
+    client_cmdline = client_message.lstrip().split(" ")
 
-    # input:  clientMsg = "/all hello world"
-    # output: clientCommand = ["/all", "hello world"]
-    # input:  clientMsg = "/msg Deckard hello world again"
-    # output: clientCommand = ["/msg", "Deckard", "hello world again"]
-    if clientCommand[0] == "/all" or clientCommand[0] == "/msg":
-        clientCommand = concatMessage(clientCommand)
+    # Split command line into command and arguments
+    client_cmd = client_cmdline[0]
+    client_args = client_cmdline[1:]
 
-    #if errorCheck(clientCommand):
+    if client_cmd == "/all":
+        try:
+            client_args = replace_emojis(client_args)
+            client_args[0] = ' '.join(client_args)
+            client_args = [arg for i,arg in enumerate(client_args) if i==0] 
+        except: 
+            client_args = [""]
+    elif client_cmd == "/msg":
+        if len(client_args) >= 2:
+            client_args = replace_emojis(client_args)
+            client_args = list([client_args[0], " ".join(client_args[1:])])
+        else:
+            print("Error: Command parameters do not match or is not allowed.")
+            continue
+
+    # Check command line for errors
+    if not error_check(client_cmd, client_args):
+        continue
+
+    # Parse commands
+    if client_cmd == "/join":
+        # Save server details
+        server_addr = client_args[0]
+        server_port = int(client_args[1])
+
+        # Join server
+        send_to_server('join')
+
+        # Wait for join confirmation from server
+        time.sleep(0.25)
+
+        # If no received confirmation from server 
+        if not connected_to_server:
+            server_addr = None
+            server_port = None
+            print("Error: Server does not exist.")
 
 
-    if clientCommand[0] == "/join":
-        server_IP = clientCommand[1]
-        server_port = clientCommand[2]
+    elif client_cmd == "/register":
+        send_to_server("register", handle=client_args[0])
+    elif client_cmd == "/all":
+        send_to_server("all", msg=client_args[0])
+    elif client_cmd == "/msg":
+        send_to_server("msg", handle=client_args[0], msg=client_args[1])
+    elif client_cmd == "/?":
+        print("""
+        
++---------------------------------------+------------------------------+-----------------------------+
+|                Command                |        Input Syntax          |     Sample Input Script     |
++---------------------------------------+------------------------------+-----------------------------+
+| Connect to a server                   | /join <server_ip_add> <port> | /join 127.0.0.1 12345       |
+| Disconnect from the server            | /leave                       | /leave                      |
+| Register a unique handle or alias     | /register <handle>           | /register Jacob             |
+| Send a message to all users in server | /all <message>               | /all Everyone can see this. |
+| Send a direct message to a user       | /msg <handle> <message>      | /msg Jacob Hello jacob.     |
+| View commands                         | /?                           | /?                          |
+| View emoji list                       | /emojis                      | /emojis                     |
++---------------------------------------+------------------------------+-----------------------------+
 
 
-    if clientCommand[0] == "/leave":
-        print("Connection closed. Thank you!")
-        exit()
-    # else:
-        # clientSocket.sendto(f"{clientName}: {clientMsg}".encode(), (server_IP, server_port))
+        """)
+    elif client_cmd == "/emojis":
+        print("""
+        Insert the emojis into your message!
++---------+-------------+
+| Command |    Emoji    |
++---------+-------------+
+| :heart: | <3          |
+| :rat:   | <:3~        |
+| :sad:   | :c          |
+| :happy: | c:          |
+| :blush: | ૮ ˶ᵔ ᵕ ᵔ˶ ა |
+| :angel: | ପ(๑•ᴗ•๑)ଓ ♡ |
+| :pout:  | ૮₍ ˃ ⤙ ˂ ₎ა |
++---------+-------------+
+
+
+        """)
+    elif client_cmd == "/leave":
+        # Delete connection details
+        
+        send_to_server("leave")
+        server_addr = None
+        server_port = None
--- udp_server.py
+++ udp_server.py
@@ -7,6 +7,7 @@ import queue
 class client:
     def __init__(self, addr: tuple, handle: str = None):
         self.addr = addr
+        self.handle = handle
 
 
 # Messages and list of clients
@@ -20,9 +21,11 @@ bufferSize = 1024
 
 
 # Server details and socket binding
-server_addr = socket.gethostname()
-server_port = 20001
+# server_addr = socket.gethostname()
+server_addr = '127.0.0.1'
+server_port = 12345
 server_sock.bind((server_addr, server_port))
+print("Server is running")
 
 
 def recv_from_clients():
@@ -37,7 +40,9 @@ def recv_from_clients():
 def create_reply(msg: str, addr: tuple) -> dict:
     global client_list
     
-    reply = {}
+    reply = {}        
+
+    current_client = next((client for client in client_list if client.addr == addr), None)
     
     # Get message in dict format
     msg_json = json.loads(msg)
@@ -45,20 +50,57 @@ def create_reply(msg: str, addr: tuple) -> dict:
     # Get command from message
     cmd = msg_json["command"]
 
-    # Receive /join from client
     if cmd == "join":
-        if any(client for client in client_list if client.addr == addr):
+        if current_client is not None:
             reply["command"] = "error"
             reply["message"] = "Current client has already joined the server."
         else:
+            client_list.append(client(addr))
+            print("Client with address {0} has been added to the client list.".format(addr))
             reply["command"] = "join"
     elif cmd == "leave":
-        if any(client for client in client_list if client.addr == addr):
-            # NOTE: Replaces the existing client_list with a new object 
-            client_list = [client for client in client_list if client.addr != addr]
-            reply["command"] = "leave"
-
-        #TODO: Make replies for all other commands, figure out how to send to recipient
+        # NOTE: Replaces the existing client_list with a new object 
+        client_list = [client for client in client_list if client.addr != addr]
+        print("Client with address {0} has disconnected, removing from client list.".format(addr))
+        reply["command"] = "leave"
+    elif cmd == "register":
+        try:
+            if any(client for client in client_list if client.handle == msg_json["handle"]):
+                reply["command"] = "error"
+                reply["message"] = "Registration failed. Handle or alias already exists."
+            elif current_client.handle is None:
+                current_client.handle = msg_json["handle"]
+                print("Client with address {0} has registered the handle {1}.".format(addr, current_client.handle))
+                reply["command"] = "register"
+                reply["handle"] = msg_json["handle"]
+            else:
+                reply["command"] = "error"
+                reply["message"] = "A handle has already been registered."
+            
+            
+        except:
+            pass
+    elif cmd == "all":
+        if current_client.handle is not None:
+            reply["command"] = "all"
+            reply["message"] = msg_json["message"]
+            reply["handle"] = current_client.handle
+        else:
+            reply["command"] = "error"
+            reply["message"] = "Cannot send messages without registering a handle."
+    elif cmd == "msg":
+        if current_client.handle is not None:
+            if not any(client for client in client_list if client.handle == msg_json["handle"]):
+                reply["command"] = "error"
+                reply["message"] = "Handle or alias not found."
+            else:
+                # Reply is for the message recipient, not the sender 
+                reply["command"] = "msg"
+                reply["handle"] = current_client.handle
+                reply["message"] = msg_json["message"]
+        else:
+            reply["command"] = "error"
+            reply["message"] = "Cannot send messages without registering a handle."
 
     return reply
 
@@ -68,20 +110,28 @@ def send_to_clients():
         while not msg_queue.empty():
             current_msg, current_addr = msg_queue.get()
 
-            if not any(client for client in client_list if client.addr == current_addr):
-                client_list.append(client(current_addr))
+            reply = create_reply(current_msg, current_addr)
 
-            """ for client in client_list:
-                try:
-                    if current_msg.decode("utf-8").startswith("SIGNUP_TAG:"):
-                        clientName = current_msg.decode()[current_msg.decode().index(":") + 1 : ]
-                        server_sock.sendto(f"Welcome {clientName}!".encode(), client)
-                    else:
-                        server_sock.sendto(current_msg, client)
-                except:
-                    client_list.remove(client) """
+            reply_json = json.dumps(reply)
 
-            # Call create_reply(), turn return val into JSON with json.dumps, sendto client
+            # Sending replies to respective recipients
+            if reply["command"] in ["error", "join", "leave", "register"]:
+                server_sock.sendto(reply_json.encode(), current_addr)
+            elif reply["command"] == "all":
+                for client in client_list:
+                    if client.handle is not None:
+                        server_sock.sendto(reply_json.encode(), client.addr)
+            elif reply["command"] == "msg":
+                # Echo the original message back to the sender client
+                server_sock.sendto(current_msg.encode(), current_addr)
+                
+                # Since handle of the reply is the sender instead of the recipient,
+                # need to get the recipient handle from the message
+                msg_json = json.loads(current_msg)
+                recipient_handle = msg_json["handle"]
+                recipient_addr = next(client for client in client_list if client.handle == recipient_handle).addr
+                server_sock.sendto(reply_json.encode(), recipient_addr)
+                
 
 
 recv_thread = threading.Thread(target = recv_from_clients)
